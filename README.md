fa6d0727d7502bdceb68e8c96586fbb978c4017e
sunmingshi <sunmingshi@baidu.com>
2018-04-19 14:51:55 +0800

    增加示例代码，后面这种方式肯定要改

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ nio网络模型
 
 ## 编译
 > cd 到naxx目录下，执行 mvn package，执行 java -jar NAXX.jar
+
+## 使用
+> 目前在 controller 包内，有一个helloworld处理器，接收到对应的url，并返回 helloworld
+> 需要在启动操作中，注册Controller到Context中
---
+++ src/main/java/com/megacreep/naxx/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.megacreep.naxx.controller;
+
+import com.google.gson.JsonObject;
+import com.megacreep.naxx.api.Controller;
+import com.megacreep.naxx.api.Request;
+import com.megacreep.naxx.http.HttpRequest;
+
+public class HelloWorldController extends Controller {
+
+    public String hello(HttpRequest req) {
+        return "hello";
+    }
+
+    @Request("/helloworld")
+    public JsonObject hellojson(HttpRequest req) {
+        JsonObject json = new JsonObject();
+        json.addProperty("msg", "hello");
+        return json;
+    }
+}
